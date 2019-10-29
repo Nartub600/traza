a746a1847e1fe43396cd8414a6a2a39861f63c33
Nartub600 <maimar@gmail.com>
2019-10-29 17:23:38 -0300

    download address

--- resources/views/trazas/ver.blade.php
+++ resources/views/trazas/ver.blade.php
@@ -88,7 +88,7 @@
     <div class="flex flex-wrap -mx-4">
       @foreach($traza->files as $file)
         <div class="w-full px-4">
-          <a href="{{ url($file['file']) }}" download="{{ $file['name'] }}">
+          <a href="{{ asset($file['file']) }}" download="{{ $file['name'] }}">
             @lang('traza.' . $file['type'])
           </a>
         </div>
